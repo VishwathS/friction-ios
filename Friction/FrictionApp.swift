9e2014e73c9bb815c1b3b32038dca10bae1048d6
VishwathS <vishwath@ucsb.edu>
2025-12-17 11:31:16 -0800

    Initial Commit

---
+++ Friction/FrictionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrictionApp.swift
+//  Friction
+//
+//  Created by Archana Shrishti Shankar on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct FrictionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
